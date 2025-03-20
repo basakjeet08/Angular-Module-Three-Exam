8efb1628e11b2dfb463fcc4af62a9fa008c814a3
Anirban Basak <jeetbasak2002@gmail.com>
2025-03-20 16:45:29 +0530

    feat : Configured the internal routing of the admin module.

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -6,14 +6,25 @@ import { FormsModule } from '@angular/forms';
 import { SharedModule } from '../shared/shared.module';
 import { authGuard } from '../shared/guard/auth.guard';
 import { adminGuard } from './guard/admin.guard';
+import { IncidentsComponent } from './pages/incidents/incidents.component';
+import { AssignComponent } from './pages/assign/assign.component';
 
 // These are the routes for the admin page
 const adminRoutes: Routes = [
-  { path: '', component: AdminComponent, canActivate: [authGuard, adminGuard] },
+  {
+    path: '',
+    component: AdminComponent,
+    canActivate: [authGuard, adminGuard],
+    children: [
+      { path: '', redirectTo: 'incidents', pathMatch: 'full' },
+      { path: 'incidents', component: IncidentsComponent },
+      { path: 'assign', component: AssignComponent },
+    ],
+  },
 ];
 
 @NgModule({
-  declarations: [AdminComponent],
+  declarations: [AdminComponent, IncidentsComponent, AssignComponent],
   imports: [
     CommonModule,
     RouterModule.forChild(adminRoutes),
