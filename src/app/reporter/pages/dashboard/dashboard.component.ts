cf99d3e9b2f47313ad5908e1af73f52d9a86b338
Anirban Basak <jeetbasak2002@gmail.com>
2025-03-20 15:29:16 +0530

    feat : Implemented the report and dashboard Page UI.

--- src/app/reporter/pages/dashboard/dashboard.component.ts
+++ src/app/reporter/pages/dashboard/dashboard.component.ts
@@ -1,8 +1,22 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css'],
 })
-export class DashboardComponent {}
+export class DashboardComponent {
+  // Injecting the necessary dependencies
+  constructor(private router: Router, private route: ActivatedRoute) {}
+
+  // This function is invoked when the user clicks on the view Incident List component
+  onIncidentPageClick() {
+    this.router.navigate(['../', 'incidents'], { relativeTo: this.route });
+  }
+
+  // This function is invoked when the user clicks on the report incident component
+  onReportIncidentClick() {
+    this.router.navigate(['../', 'report'], { relativeTo: this.route });
+  }
+}
--- src/app/reporter/pages/report/report.component.ts
+++ src/app/reporter/pages/report/report.component.ts
@@ -1,3 +1,4 @@
+import { Location } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
@@ -5,4 +6,36 @@ import { Component } from '@angular/core';
   templateUrl: './report.component.html',
   styleUrls: ['./report.component.css'],
 })
-export class ReportComponent {}
+export class ReportComponent {
+  // This is the user data for the component
+  userInput = {
+    id: '',
+    title: '',
+    priority: '',
+    reportedBy: '',
+    status: 'OPEN',
+  };
+
+  // These are the loading and error state variables
+  isLoading: boolean = false;
+  errorMessage: string | null = null;
+  isEditMode: boolean = false;
+
+  // Injecting the required dependencies
+  constructor(private location: Location) {}
+
+  // This function is invoked when the user clicks on the submit button
+  onSubmitClick() {
+    console.log(this.userInput);
+  }
+
+  // This function is invoked when the user clicks on the cancel button
+  onCancelClick() {
+    this.location.back();
+  }
+
+  // This function is invoked when the user clicks on the cancel button in the error card
+  onErrorCancelClick() {
+    this.errorMessage = null;
+  }
+}
