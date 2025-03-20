6b673db072ac00cc726ef9ce647bf90aeb66bdab
Anirban Basak <jeetbasak2002@gmail.com>
2025-03-20 15:13:16 +0530

    feat : Configured the internal routing of the reporter module.

--- src/app/reporter/reporter.module.ts
+++ src/app/reporter/reporter.module.ts
@@ -6,6 +6,9 @@ import { FormsModule } from '@angular/forms';
 import { SharedModule } from '../shared/shared.module';
 import { authGuard } from '../shared/guard/auth.guard';
 import { reporterGuard } from './guard/reporter.guard';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { ReportComponent } from './pages/report/report.component';
+import { IncidentsComponent } from './pages/incidents/incidents.component';
 
 // These are the reported module routes
 const reporterRoutes: Routes = [
@@ -13,11 +16,22 @@ const reporterRoutes: Routes = [
     path: '',
     component: ReporterComponent,
     canActivate: [authGuard, reporterGuard],
+    children: [
+      { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+      { path: 'dashboard', component: DashboardComponent },
+      { path: 'report', component: ReportComponent },
+      { path: 'incidents', component: IncidentsComponent },
+    ],
   },
 ];
 
 @NgModule({
-  declarations: [ReporterComponent],
+  declarations: [
+    ReporterComponent,
+    DashboardComponent,
+    ReportComponent,
+    IncidentsComponent,
+  ],
   imports: [
     CommonModule,
     RouterModule.forChild(reporterRoutes),
